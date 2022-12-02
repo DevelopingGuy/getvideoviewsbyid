bf07bd93b9b53a7597f9487de13f2e8bda3f0aed
DevelopingGuy <70748640+DevelopingGuy@users.noreply.github.com>
2022-12-02 11:00:52 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+# Imports
+from TikTokApi import TikTokApi
+import sys
 
+# Declare the API
+api = TikTokApi()
+
+# Get argument 1 from command line
+idraw = sys.argv[1]
+
+# Get the starts of the video binded with the ID given
+id = api.video(id=idraw).stats
+
+# Turn dictionary into variables
+locals().update(id)
+
+# Print the video views
+print(playCount)
